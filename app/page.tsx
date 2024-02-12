3523d91746c5b03f23259a983b4084953faab937
Chandra Bose <chandrabosep3112@gmail.com>
2024-02-12 23:53:19 +0530

    style: homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,87 @@
+import { Button } from "@/components/ui/button";
+import { Sparkles } from "lucide-react";
+import Image from "next/image";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <Image
+        src="/hero.png"
+        alt="hero"
+        width={5000}
+        height={5000}
+        className="object-cover  h-screen flare contrast-50 absolute -z-10"
+      />
+      <div className="w-full h-screen flex flex-col justify-center items-center gap-12">
+        <div className="w-full flex flex-col gap-4 justify-center items-center">
+          <Image
+            src={"/logo.svg"}
+            alt="hero"
+            width={5000}
+            height={5000}
+            className="w-4/12"
+          />
+          <p className="text-2xl font-bold">
+            Find work, review jobs or hire talent.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 justify-center items-center">
+          <p className="text-lg py-4 font-bold ">
+            Anonymous, secure and always free to use
+          </p>
+          <Button className="flex items-center w-fit gap-1 px-3 py-1 ring-1 ring-white text-green-500 bg-grad-magic rounded-full shadow-md">
+            <Sparkles className="w-5 h-5" strokeWidth={1.5} />
+            Join the HEROES
+          </Button>
+        </div>
+      </div>
+      <div className="w-10/12 m-auto">
+        <h1 className="text-4xl font-bold text-center py-8">
+          Choose your HERO role
+        </h1>
+        <div className="flex justify-between gap-4">
+          {[
+            {
+              title: "Reviewer",
+              content1: "Earn reviewing freelancers jobs.",
+              content2: "Connect with like minded people.",
+              img: "/reviewer.jpg",
+            },
+            {
+              title: "Freelance",
+              content1:
+                "Trustless and secure payments for your delivered jobs.",
+              content2: "Get instant feedback from experts.",
+              img: "/freelancer.jpg",
+            },
+            {
+              title: "Reviewer",
+              content1: "Hire the best talent, get jobs securely delivered.",
+              content2: "Get jobs reviewed by experts.",
+              img: "/Employer.jpg",
+            },
+          ].map(({ title, content1, content2, img }) => (
+            <div className="w-4/12 bg-grad-magic rounded-3xl py-2 px-1 ">
+              <p className="text-2xl font-medium text-center py-2">{title}</p>
+              <div className="bg-mint-200 flex flex-col justify-between h-[90%] w-[99%] m-auto rounded-3xl pt-4">
+                <div className="w-10/12 mx-auto flex flex-col justify-between h-full mb-0 pb-0 gap-8">
+                  <div>
+                    <li>{content1}</li>
+                    <li>{content2}</li>
+                  </div>
+                  <Image
+                    src={img}
+                    alt="hero"
+                    width={5000}
+                    height={5000}
+                    className="fit rounded-t-full w-8/12  mx-auto h-64 contrast-50 "
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { MessageCircleMore } from "lucide-react";
+import { MessageCircleMore, Sparkle, Sparkles } from "lucide-react";
 import Image from "next/image";
 import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { useAccount } from "wagmi";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 type address = string | undefined | null;
 
@@ -13,6 +14,8 @@ export default function Navbar() {
   const [current, setCurrent] = useState(
     `text-mint-100 bg-dark-800 px-3 py-1.5 rounded-full`
   );
+  const path = usePathname();
+  const router = useRouter();
 
   const {
     isConnected,
@@ -34,25 +37,43 @@ export default function Navbar() {
     <div className="max-w-screen-3xl w-11/12 m-auto py-4 flex justify-between border-b-[3px] border-dark-800">
       <div className="flex items-center gap-4">
         <Image src={"/logo.svg"} alt="logo" width={120} height={120} />
-        <span className={`${current}`}>Jobs Open</span>
-        <span className={`${current}`}>Jobs to Review</span>
-        <span className={`${current}`}>Post a Job</span>
+        {path === "/" ? (
+          ""
+        ) : (
+          <>
+            <span className={`${current}`}>Jobs Open</span>
+            <span className={`${current}`}>Jobs to Review</span>
+            <span className={`${current}`}>Post a Job</span>{" "}
+          </>
+        )}
       </div>
       <div className="flex gap-4">
-        <Link
-          href={"/global-chat"}
-          className="flex items-center gap-1 px-3 py-1 text-green-500 bg-grad-magic rounded-full shadow-md"
-        >
-          <MessageCircleMore className="w-5 h-5" /> Chat
-        </Link>
+        {path === "/" ? (
+          <Link
+            href={"/jobs"}
+            className="flex items-center gap-1 px-3 py-1 text-green-500 bg-grad-magic rounded-full shadow-md"
+          >
+            <Sparkles className="w-5 h-5" strokeWidth={1.2} />
+            Be Heroic
+          </Link>
+        ) : (
+          <>
+            <Link
+              href={"/global-chat"}
+              className="flex items-center gap-1 px-3 py-1 text-green-500 bg-grad-magic rounded-full shadow-md"
+            >
+              <MessageCircleMore className="w-5 h-5" /> Chat
+            </Link>
 
-        <Button
-          onClick={() => open()}
-          className="w3m-account-button text-md text-green-500 px-5 gap-2"
-        >
-          <Image src={"/contract.svg"} alt="logo" width={18} height={18} />
-          {isConnected ? extractLetters() : "connect Wallet"}
-        </Button>
+            <Button
+              onClick={() => open()}
+              className="w3m-account-button text-md text-green-500 px-5 gap-2"
+            >
+              <Image src={"/contract.svg"} alt="logo" width={18} height={18} />
+              {isConnected ? extractLetters() : "connect Wallet"}
+            </Button>
+          </>
+        )}
       </div>
     </div>
   );
